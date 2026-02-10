37a6d721da9c0952e12167c1bf331303c24ed01f
DESKTOP-DDFM8JA\aayus <aayush.dahiya@jaiwebsoft.com>
2026-02-10 13:28:50 +0530

    new change

--- db.js
+++ db.js
@@ -1,11 +1,10 @@
-const Pool = require("pg").Pool;
+const { Pool } = require("pg");
 
 const pool = new Pool({
-    user: "postgres",
-    password: "aayushsql",
-    host: "localhost",
-    port: 5432,
-    database: "perntodo"
-})
+  connectionString: process.env.DATABASE_URL || "postgresql://postgres:aayushsql@localhost:5432/perntodo",
+  ssl: process.env.DATABASE_URL
+    ? { rejectUnauthorized: false }
+    : false
+});
 
 module.exports = pool;
